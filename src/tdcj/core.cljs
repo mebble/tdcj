8bac378e051d9b1a738e6fe08a56bb2600cbe513
Neil Syiemlieh <neilsyiemlieh@gmail.com>
2022-12-05 22:14:30 +0530

    init project with re-frame lein template

---
+++ src/tdcj/core.cljs
@@ -0,0 +1,24 @@
+(ns tdcj.core
+  (:require
+   [reagent.dom :as rdom]
+   [re-frame.core :as re-frame]
+   [tdcj.events :as events]
+   [tdcj.views :as views]
+   [tdcj.config :as config]
+   ))
+
+
+(defn dev-setup []
+  (when config/debug?
+    (println "dev mode")))
+
+(defn ^:dev/after-load mount-root []
+  (re-frame/clear-subscription-cache!)
+  (let [root-el (.getElementById js/document "app")]
+    (rdom/unmount-component-at-node root-el)
+    (rdom/render [views/main-panel] root-el)))
+
+(defn init []
+  (re-frame/dispatch-sync [::events/initialize-db])
+  (dev-setup)
+  (mount-root))
---
+++ src/tdcj/db.cljs
@@ -0,0 +1,4 @@
+(ns tdcj.db)
+
+(def default-db
+  {:name "re-frame"})
