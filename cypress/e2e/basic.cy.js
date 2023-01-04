3aaec335be30ab3f42e3695c514ff9abd00b049b
Neil Syiemlieh <neilsyiemlieh@gmail.com>
2023-01-04 10:12:47 +0530

    cypress test localstorage for add, delete, edit and check actions

--- cypress/e2e/basic.cy.js
+++ cypress/e2e/basic.cy.js
@@ -23,12 +23,26 @@ describe('basics', () => {
             .should('have.length', 3)
             .last()
             .should('have.text', todo)
+            .should(() => {
+                const expectedMeta = '["todo:1" "todo:2" "todo:3"]'
+                expect(localStorage.getItem('meta:ids')).to.eq(expectedMeta)
+                expect(localStorage.getItem('todo:1')).to.not.be.null
+                expect(localStorage.getItem('todo:2')).to.not.be.null
+                expect(localStorage.getItem('todo:3')).to.not.be.null
+            })
 
         cy.get('[data-delete=1]').click()
         cy.get('#todo-list li')
             .should('have.length', 2)
             .first()
             .should('have.text', existingTodos[1])
+            .should(() => {
+                const expectedMeta = '["todo:2" "todo:3"]'
+                expect(localStorage.getItem('meta:ids')).to.eq(expectedMeta)
+                expect(localStorage.getItem('todo:1')).to.be.null
+                expect(localStorage.getItem('todo:2')).to.not.be.null
+                expect(localStorage.getItem('todo:3')).to.not.be.null
+            })
     })
 
     it('checks off a todo', () => {
@@ -37,27 +51,56 @@ describe('basics', () => {
             .should('have.length', 2)
             .first()
             .should('have.attr', 'data-is-done', "true")
+            .should(() => {
+                const expectedMeta = '["todo:1" "todo:2"]'
+                expect(localStorage.getItem('meta:ids')).to.eq(expectedMeta)
+                expect(localStorage.getItem('todo:1')).to.contain(':done true')
+                expect(localStorage.getItem('todo:2')).to.contain(':done false')
+            })
+
+        cy.get('[data-done=1]').click()
+        cy.get('#todo-list li')
+            .should('have.length', 2)
+            .first()
+            .should('have.attr', 'data-is-done', "false")
+            .should(() => {
+                const expectedMeta = '["todo:1" "todo:2"]'
+                expect(localStorage.getItem('meta:ids')).to.eq(expectedMeta)
+                expect(localStorage.getItem('todo:1')).to.contain(':done false')
+                expect(localStorage.getItem('todo:2')).to.contain(':done false')
+            })
     })
 
     it('edits a todo', () => {
+        const oldText = existingTodos[0];
         const newText = 'Feed the dog';
 
         cy.get('[data-edit=1]').click()
             .should('be.checked')
             .parent()
-            .should('not.have.text', existingTodos[0])
+            .should('not.have.text', oldText)
 
         cy.get('[data-input=1]')
-            .should('have.value', existingTodos[0])
+            .should('have.value', oldText)
             .type('{backspace}{backspace}{backspace}dog')
 
         cy.get('[data-input=1]')
             .should('have.value', newText)
+            .should(() => {
+                const expectedMeta = '["todo:1" "todo:2"]'
+                expect(localStorage.getItem('meta:ids')).to.eq(expectedMeta)
+                expect(localStorage.getItem('todo:1')).to.contain(`:txt "${oldText}"`)
+            })
 
         cy.get('[data-edit=1]').click()
             .should('not.be.checked')
             .parent()
             .should('have.text', newText)
+            .should(() => {
+                const expectedMeta = '["todo:1" "todo:2"]'
+                expect(localStorage.getItem('meta:ids')).to.eq(expectedMeta)
+                expect(localStorage.getItem('todo:1')).to.contain(`:txt "${newText}"`)
+            })
 
         cy.get('[data-input=1]')
             .should('not.exist')
