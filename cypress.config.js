d311db1846ff8545a49021c5c7e26aa385bc0e00
Neil Syiemlieh <neilsyiemlieh@gmail.com>
2023-01-04 15:48:06 +0530

    set up shadow-cljs unit tests and cypress headless test script

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,8 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  video: false,
+  screenshotOnRunFailure: false,
   e2e: {
     baseUrl: 'http://localhost:8280',
     setupNodeEvents(on, config) {
---
+++ test/tdcj/events_test.cljs
@@ -0,0 +1,5 @@
+(ns tdcj.events-test
+  (:require [cljs.test :refer [deftest is]]))
+
+(deftest test-one
+  (is (= 4 (+ 2 2))))
