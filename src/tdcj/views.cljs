5bfd9ee85bf1b2d0469533ff9e159ce82c39d70b
Neil Syiemlieh <neilsyiemlieh@gmail.com>
2023-01-18 13:23:47 +0530

    add prompt when no todos present

--- src/tdcj/views.cljs
+++ src/tdcj/views.cljs
@@ -59,14 +59,15 @@
    [:h1.text-4xl.text-center.mb-4
     [:span.font-black "Todo"]
     [:span.font-extralight "App"]]
-   [:ul#todo-list.border.border-black.rounded-sm.divide-y.divide-black.mb-4
-    ;; We can subscribe to each todo:
-    ;; https://github.com/reagent-project/reagent/issues/18#issuecomment-51316043
-    (doall (for [i (range @(rf/subscribe [::subs/num-todos]))]
-             (let [t @(rf/subscribe [::subs/todo i])]
-               ^{:key (:id t)} [todo-item i t])))
-
-    ;; Alternatively we can subscribe to all todos:
-    #_(for [t @(rf/subscribe [::subs/todos])]
-        ^{:key (:id t)} [:li {:data-id (:id t)} (:txt t)])]
+   (if (zero? @(rf/subscribe [::subs/num-todos]))
+     [:p.text-center.h-16.mb-4.flex.flex-col.justify-center "Add a todo below"]
+     [:ul#todo-list.border.border-black.rounded-sm.divide-y.divide-black.mb-4
+      ;; We can subscribe to each todo:
+      ;; https://github.com/reagent-project/reagent/issues/18#issuecomment-51316043
+      (doall (for [i (range @(rf/subscribe [::subs/num-todos]))]
+               (let [t @(rf/subscribe [::subs/todo i])]
+                 ^{:key (:id t)} [todo-item i t])))
+    ;; Alternatively we can subscribe to all todos: 
+      #_(for [t @(rf/subscribe [::subs/todos])]
+          ^{:key (:id t)} [:li {:data-id (:id t)} (:txt t)])])
    [new-todo]])
